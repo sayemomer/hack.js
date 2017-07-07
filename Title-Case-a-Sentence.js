5a8290b1fd9eb0c9d0253aac66cf6e4848d01301
omersayem007 <omer_sayem@yahoo.com>
2017-07-07 21:24:23 +0600

    clean

--- Title-Case-a-Sentence.js
+++ Title-Case-a-Sentence.js
@@ -1,7 +1,8 @@
 function titleCase(str) {
   
-var newstr=str.split(" ").map( (x)=>x[0] == x[0].toUpperCase() ? x.toLowerCase().replace(x[0].toLowerCase(),x[0].toUpperCase()) : x.toLowerCase().replace(x[0],x[0].toUpperCase()) );
-  
+var newstr=str.split(" ").map( (x)=>x[0] == x[0].toUpperCase() ? x.toLowerCase().replace(x[0].toLowerCase(),x[0].toUpperCase()) : x.replace(x[0],x[0].toUpperCase()) );
+/* compare the first element of the string doing the upper case ! , if true lowercase the whole string
+  and replace the 1st element  with the upper case . other wase*/
 return newstr.join(" ");
   
   
