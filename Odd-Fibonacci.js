6b7a2e3179f6234784f9fa131d2883918ddd4fc3
omersayem007 <omer_sayem@yahoo.com>
2017-07-07 20:04:29 +0600

    clean

--- Odd-Fibonacci.js
+++ Odd-Fibonacci.js
@@ -13,3 +13,25 @@ function sumFibs(range ){
 }
 
 sumFibs(10);
+
+//  ---------------------------Lodash implimentation---------------------------------//
+
+var _ = require('lodash');
+function sumFibs(range ){
+  var x =[0,1];
+  
+  for( let i=2; ;i++){
+    
+    if(x[x.length-1]>range){
+     x.pop();
+     break;
+   }
+     x.push( x[i-1]+x[i-2] )
+  }
+  let filarray= x.filter( (x)=> x%2!==0 );
+  
+  return _.sum(filarray);
+}
+
+sumFibs(10);
+
