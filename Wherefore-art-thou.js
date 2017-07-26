08477cdf7d7441dae682d1f8b3159faefae2c563
omersayem007 <omer_sayem@yahoo.com>
2017-07-26 23:11:57 +0600

    clean like hell

---
+++ Wherefore-art-thou.js
@@ -0,0 +1,34 @@
+function whatIsInAName(collection, source) {
+   
+   let resultArray=[];
+   let flag=0 ;
+ 
+ for( let i=0 ;i<collection.length;i++){
+   
+   for(let j=0 ;j< Object.keys(collection[i]).length ;j++){
+     
+     for( let k=0 ;k<Object.keys(source).length ;k++){
+       
+       if( Object.values( collection[i] )[j]==Object.values(source)[k]  && Object.keys( collection[i] )[j]==Object.keys(source)[k] ){
+         
+         
+       flag++;
+     }
+     
+     }
+     
+     
+   }
+   console.log(flag);
+   
+   if( flag === Object.keys(source).length){
+     resultArray.push( collection[i] );
+   }
+   flag=0;
+ }
+  
+  return resultArray ;
+
+}
+
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
