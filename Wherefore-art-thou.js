f447757b67ad63ed0a207f6884320f926f041254
omersayem007 <omer_sayem@yahoo.com>
2017-07-26 23:14:19 +0600

    clean like hell

--- Wherefore-art-thou.js
+++ Wherefore-art-thou.js
@@ -32,3 +32,6 @@ function whatIsInAName(collection, source) {
 }
 
 whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 })
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 })
