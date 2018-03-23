d08a163fa72d485315750024cbdb5224cedb005d
osOmer <omer_sayem@yahoo.com>
2018-03-24 03:48:26 +0600

    Update Inventory-Update.js

--- Inventory-Update.js
+++ Inventory-Update.js
@@ -1,22 +1,40 @@
 
-function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-    return arr1;
+function updateInventoryHelper(newInv,curInv){
+  
+  
+  if(newInv[1]===curInv[1]){
+            
+            if( newInv[0] > curInv[0] ){
+              var flag=1;
+            curInv[0] = newInv[0];
+            }
+            
+          }
+  return flag;
+}
+
+function updateInventory(curInv, newInv) {
+   var check;
+  
+    
+     for( var i=0 ;i<newInv.length;i++){
+       
+        for( var j=0 ;j<curInv.length;j++){
+          check= updateInventoryHelper(newInv[i],curInv[j]);
+          if(check===1 ){
+            break;
+          }
+        }
+        
+        if( check === undefined){
+          curInv.push(newInv[i]);
+        }
+        
+     }
+     
+    return curInv;
 }
 
 // Example inventory lists
-var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
-];
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]] , [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]])
 
-var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
-];
-
-updateInventory(curInv, newInv);
