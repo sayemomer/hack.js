c33f28404b49d00aa2eaded38381e36e3485db9d
osOmer <omer_sayem@yahoo.com>
2018-06-06 01:32:30 +0600

    Update Smallest-Common-Multiple.js

--- Smallest-Common-Multiple.js
+++ Smallest-Common-Multiple.js
@@ -1,19 +1,74 @@
-function smallestCommons(arr) {
-  var arr1=[];
-  var arr2=[];
-  
-  for( let i=1;i<=Math.max(...arr);i++ ){
-    
-    arr1.push(i*arr[0]);
-    arr2.push(i*arr[1]);
-    
-    
-  }
+function getMultiples(firstValue, secondValue){
+var multipliers1 =[];
+var multipliers2 =[];
+for( var i=1 ;i<=10000;i++){
+  multipliers1.push(firstValue*i);
+  multipliers2.push(secondValue*i)
+}
+return [multipliers1,multipliers2];
+}
 
- 
- return Math.max( ...arr1.map( (x)=> arr2.filter( (y)=> x===y ? x :0) ) );
- 
+function checkAvailable(minedNumber,firstValue, secondValue){
+
+  var flag =0 ;
+  var num=0;
+
+  for( var i=firstValue ; i<=secondValue;i++){
+      if( minedNumber%i ==0 ){
+        flag++;
+        
+      }
+      num++;
+  }
+  
+return  flag=== num ? "ok":"nope";
+
 }
 
 
-smallestCommons([1,5]);
+function findCommon(multipliers, firstValue, secondValue){
+var  firstDataArray =multipliers[0];
+var  secondDataArray =multipliers[1];
+var result=0;
+
+for( var i=0 ;i<firstDataArray.length ;i++){
+
+for( var j=0 ;j<secondDataArray.length;j++){
+
+  if( firstDataArray[i]=== secondDataArray[j]){
+    
+    if ( checkAvailable( firstDataArray[i],firstValue,secondValue) === "ok")  {
+      result = firstDataArray[i];
+      break;
+
+    }
+  }
+
+}
+}
+
+return result ;
+
+}
+
+function smallestCommons(arr) {
+  var largestValue;
+  var smallValue ;
+
+  if( arr[0]> arr[1]){
+
+    largestValue=arr[0];
+    smallValue = arr[1];    
+  }
+  else{
+    largestValue =arr[1];
+    smallValue = arr[0];
+  }
+
+  
+
+  return findCommon(getMultiples(arr[0],arr[1]) ,smallValue,largestValue ); 
+}
+
+
+smallestCommons([1, 13]);
